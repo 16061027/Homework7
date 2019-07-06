09bcea7959b5ccb3af677e92b373fc15a7bde413
aico <249449587@qq.com>
2019-07-06 22:39:36 +0800

    first commit

--- demo/AndroidStudy/app/src/main/java/com/domker/study/androidstudy/IJKPlayerActivity.java
+++ demo/AndroidStudy/app/src/main/java/com/domker/study/androidstudy/IJKPlayerActivity.java
@@ -1,14 +1,23 @@
 package com.domker.study.androidstudy;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.SurfaceHolder;
 import android.view.View;
+import android.widget.SeekBar;
 
 import com.domker.study.androidstudy.player.VideoPlayerIJK;
 import com.domker.study.androidstudy.player.VideoPlayerListener;
+
+import java.io.File;
 
 import tv.danmaku.ijk.media.player.IjkMediaPlayer;
 
@@ -16,6 +25,10 @@ public class IJKPlayerActivity extends AppCompatActivity {
     private VideoPlayerIJK ijkPlayer;
     private MediaPlayer player;
     private SurfaceHolder holder;
+    private SeekBar skb;
+    private Handler handler;
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -34,7 +47,7 @@ public class IJKPlayerActivity extends AppCompatActivity {
         }
         ijkPlayer.setListener(new VideoPlayerListener());
         ijkPlayer.setVideoResource(R.raw.yuminhong);
-//        ijkPlayer.setVideoPath(getVideoPath());
+        //ijkPlayer.setVideoPath(getVideoPath());
 
         findViewById(R.id.buttonPlay).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,12 +63,47 @@ public class IJKPlayerActivity extends AppCompatActivity {
             }
         });
 
-        findViewById(R.id.buttonSeek).setOnClickListener(new View.OnClickListener() {
+        skb = findViewById(R.id.seekbar1);
+        skb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onClick(View v) {
-                ijkPlayer.seekTo(20 * 1000);
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if(b){
+                    //System.out.println(i + "i");
+                    //System.out.println(ijkPlayer.getDuration() + "d");
+                    float temp = (float)i / (float)100;
+                    //System.out.println(temp + "t");
+                    long loca = (long) (temp * ijkPlayer.getDuration());
+                    //System.out.println(loca + "l");
+                    ijkPlayer.seekTo(loca);
+
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         });
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                switch (msg.what) {
+                    case 1:
+                        refresh();
+                        handler.sendEmptyMessageDelayed(1, 1000);
+                        break;
+                }
+
+            }
+        };
+        handler.sendEmptyMessage(1);
+
+
     }
 
 
@@ -73,4 +121,16 @@ public class IJKPlayerActivity extends AppCompatActivity {
 
         IjkMediaPlayer.native_profileEnd();
     }
+
+    private void refresh() {
+        long current = ijkPlayer.getCurrentPosition() / 1000;
+        long duration = ijkPlayer.getDuration() / 1000;
+        if (duration != 0) {
+            skb.setProgress((int) (current * 100 / duration));
+        }
+
+    }
+
+
+
 }
